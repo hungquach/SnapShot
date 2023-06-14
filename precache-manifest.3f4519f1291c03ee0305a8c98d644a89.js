fd98b54ed2408cb9396a32856ec3d7ab218bc707
Hung Quach <hungquach12@outlook.com>
2023-06-14 22:12:27 +0700

    Updates

---
+++ precache-manifest.3f4519f1291c03ee0305a8c98d644a89.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40ab3d78f7ff7ef69ee03a879303a70f",
+    "url": "/SnapShot/index.html"
+  },
+  {
+    "revision": "3b09eed38af026689274",
+    "url": "/SnapShot/static/css/main.d03163bd.chunk.css"
+  },
+  {
+    "revision": "d181d5d7af02efcd3dab",
+    "url": "/SnapShot/static/js/2.c619e43e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/SnapShot/static/js/2.c619e43e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b09eed38af026689274",
+    "url": "/SnapShot/static/js/main.03bcbd7f.chunk.js"
+  },
+  {
+    "revision": "17f44848bce1e5215d0e",
+    "url": "/SnapShot/static/js/runtime-main.136163d9.js"
+  }
+]);
